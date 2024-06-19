c6cb04959993684ae32cb5852f2c9db638ad0537
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-06-19 08:39:49 +0000

    Add basic setup for tasks page and some templates

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // sidevanv initialization
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
+
